be6ceb2b2e96c757e845bdfb005d1ef882d688cd
Yi-Coder <toy041126@gmail.com>
2016-11-06 23:11:31 -0500

    backup

--- RS/Reservation.swift
+++ RS/Reservation.swift
@@ -14,6 +14,5 @@ class Reservation{
     
     
     //let dateformatter = NSDateFormatter()
-
     
 }
--- RS/ReservedList.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ReservedList.swift
-//  RS
-//
-//  Created by liangyi on 10/21/16.
-//  Copyright © 2016 liangyi. All rights reserved.
-//
-
-import Foundation
